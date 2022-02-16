8328193fd9955f6504ec001621250df373469f3a
arkaan27 <arkaankanunga@gmail.com>
2022-02-16 11:30:35 +0000

    Added record keeping for ingestion.py

--- ingestion.py
+++ ingestion.py
@@ -21,7 +21,7 @@ import numpy as np
 import os
 import json
 from datetime import datetime
-import logging 
+import logging
 
 # Initialising logger for checking steps
 logging.basicConfig(
@@ -30,61 +30,74 @@ logging.basicConfig(
     filemode='w',
     format='%(name)s - %(levelname)s - %(message)s')
 
-#############Load config.json and get input and output paths
-with open('config.json','r') as f:
-    config = json.load(f) 
+# Load config.json and get input and output paths
+with open('config.json', 'r') as f:
+    config = json.load(f)
 
 input_folder_path = config['input_folder_path']
 output_folder_path = config['output_folder_path']
 
 
-
 #############Function for data ingestion
 def merge_multiple_dataframe():
-    #check for datasets, compile them together, and write to an output file
+    """
+    check for datasets, compile them together, and write to an output file
+    """
 
     current_dir = os.getcwd()
-    input_path= f"{current_dir}/{input_folder_path}"
-    
+    input_path = f"{current_dir}/{input_folder_path}"
+
     # Creating the output folder if it does not exist
     if not os.path.exists(output_folder_path):
         os.makedirs(output_folder_path)
         logging.info("Creating output directory")
-        
+
     # Joining the output folder path
     output_path = os.path.join(os.getcwd, output_folder_path)
-    
+
     # Listing the files present in the input path
     files = os.listdir(input_path)
-    
+
     # Reading the data frame from the folder
-    
+
     logging.info("Reading the files with pandas module")
-    df = [pd.read_csv(os.path.join(input_path,fname) for fname in files)]
-    
+    df = [pd.read_csv(os.path.join(input_path, fname) for fname in files)]
+
+    # Getting the date and time for reference
+
+    logging.info("Getting date for record keeping")
+    dateTimeObj = datetime.now()
+    date_now = str(dateTimeObj.year) + '/' + str(dateTimeObj.month) + '/' + str(dateTimeObj.day)
+
     # Combining the files into one dataframe
-    
+
     logging.info("Combining the dataframes together")
     combined_df = pd.concat(df)
-    
+
     # Cleaning the data
-    
+
     logging.info("Dropping duplicates")
     combined_df.drop_duplicates(inplace=True)
-    
-    
+
+    # Creating all records
+
+    logging.info("Creating Records for dataframe")
+    allrecord = [input_folder_path, files, len(combined_df.index), date_now]
+
     # Exporting the dataframe to csv
-    
+
     logging.info("Exporting the dataframe to csv as finaldata.csv")
-    output_csv_path = os.path.join(output_path,"finaldata.csv")
-    combined_df.to_csv(output_csv_path,index=False)
+    output_csv_path = os.path.join(output_path, "finaldata.csv")
+    combined_df.to_csv(output_csv_path, index=False)
 
     # Writing ingested file.txt for file path
-    
-    logging.info("Adding the files to ingestedfiles text")
-    output_txt_path = os.path.join(output_path,"ingestedfiles.txt")
+
+    logging.info("Adding records to ingested_files.txt")
+    output_txt_path = os.path.join(output_path, "ingestedfiles.txt")
     with open(output_txt_path, "w") as f:
-        f.write(str(files))
+        for element in allrecord:
+            f.write(str(element))
+
 
 if __name__ == '__main__':
     merge_multiple_dataframe()
